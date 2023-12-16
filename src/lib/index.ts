38d21347e1f9113264fff0e78f35a54ab02046b6
fallenoak <git@fallenoak.me>
2023-12-16 11:25:38 -0600

    chore(blp): expose color format enum at library root

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
-import Blp, { BlpImage, BLP_IMAGE_FORMAT } from './blp/Blp.js';
+import Blp, { BlpImage, BLP_COLOR_FORMAT, BLP_IMAGE_FORMAT } from './blp/Blp.js';
 
-export { Blp, BlpImage, BLP_IMAGE_FORMAT };
+export { Blp, BlpImage, BLP_COLOR_FORMAT, BLP_IMAGE_FORMAT };
